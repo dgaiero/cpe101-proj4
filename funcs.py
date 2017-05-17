9dfb7ae42a910924d241294ba82b4edb10834dc2
dgaiero <dpgaiero@gmail.com>
2017-05-17 14:03:22 -0700

    Added file input and printing of puzzle

--- funcs.py
+++ funcs.py
@@ -1,12 +1,30 @@
-def readLetters():
-    letters = input(">>> ")
+def readFile():
+    puzzleLetters = input("")
+    puzzleWords = input("")
     print()
+    puzzleWords = puzzleWords.split(" ")
+    puzzleInformation = {
+        'puzzleWords' : puzzleWords,
+        'puzzleLetters' : puzzleLetters
+    }
+    return puzzleInformation
+
+
+def readLetters(puzzleLetters):
+    letters = puzzleLetters
     lettersList = list(letters)
+    print("Puzzle: \n")
     for i in range(0, 100, 10):
         for j in range(10):
             print(lettersList[i+j],end="")
         print()
 
 
+'''
+For testing purposes. Delete for final rev.
+'''
 if __name__ == "__main__":
-    readLetters()
+    fileInfo = readFile()
+    puzzleWords = fileInfo['puzzleWords']
+    puzzleLetters = fileInfo['puzzleLetters']
+    readLetters(puzzleLetters)
