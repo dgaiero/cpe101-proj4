b2a4956ed9b9cbafdccdfb96aea6f78263cbd498
dgaiero <dpgaiero@gmail.com>
2017-05-19 15:27:45 -0700

    PEP8 styling

--- funcs.py
+++ funcs.py
@@ -12,29 +12,34 @@ Steps:
     Add the puzzle words and puzzle letters to a dictionary
     return dictionary
 '''
+
+
 def readFile():
     puzzleLetters = input("")
     puzzleWords = input("")
     print()
     puzzleWords = puzzleWords.split(" ")
     puzzleInformation = {
-        'puzzleWords' : puzzleWords,
-        'puzzleLetters' : puzzleLetters
+        'puzzleWords': puzzleWords,
+        'puzzleLetters': puzzleLetters
     }
     return puzzleInformation
+
 
 '''
 Steps:
     Read the letters.
     Print the list into a 10x10 matrix.
 '''
+
+
 def readLetters(puzzleLetters):
     letters = puzzleLetters
     lettersList = list(letters)
     print("Puzzle: \n")
     for i in range(0, 100, 10):
         for j in range(10):
-            print(lettersList[i+j],end="")
+            print(lettersList[i + j], end="")
         print()
 
 
